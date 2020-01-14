893b57cce3095a80392c1c1359a5be515aee41c1
JamieVaughn <wxv6@case.edu>
2020-01-14 16:43:19 -0500

    Add instructions

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+We want to create a dashboard interface that allows us to input a user ID 
+and submit it to fetch a user object in JSON format which we can then use as data
+to "hydrate" the page with user profiles in some kind of "card" style
+
+You can choose to use either Objects or Classes to organize your code into a module
+
+We'll use this endpoint to fetch the users: `https://jsonplaceholder.typicode.com/users/${id}`
+
+
+the documentation for how to use that endpont is here: https://jsonplaceholder.typicode.com
+
+Bonus: Use promises, keyframes and web-animations library to make the users "card" animated onto the page nicely and gradually 
+*/
