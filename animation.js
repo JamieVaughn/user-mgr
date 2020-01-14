cbf473fcd35c15b131be9834d6f6461da7f99401
JamieVaughn <wxv6@case.edu>
2020-01-14 16:52:32 -0500

    Remove animations promises

--- animation.js
+++ animation.js
@@ -26,26 +26,4 @@ var KEYFRAMES = {
     ]
   };
   
-  var box = document.querySelector('.callback');
-  var anim1 = box.animate(KEYFRAMES.o, 1000);
-  anim1.onfinish = function() {
-    var anim2 = box.animate(KEYFRAMES.t, 1000);
-    anim2.onfinish = function() {
-      var anim3 =box.animate(KEYFRAMES.r, 1000);
-      anim3.onfinish = function() {
-        box.animate(KEYFRAMES.bb, 1000);
-      };
-    };
-  };
   
-  var box2 = document.querySelector('.promise');
-  var animA = box2.animate(KEYFRAMES.o, 1000);
-  animA.finished.then(function() {
-    var animB = box2.animate(KEYFRAMES.t, 1000);
-    return animB.finished;
-  }).then(function() {
-    var animC = box2.animate(KEYFRAMES.r, 1000);
-    return animC.finished;
-  }).then(function() {
-    box2.animate(KEYFRAMES.br, 1000);
-  });
